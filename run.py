59313ebc2fbf858607139dcfa0eb4eb7cd7580e4
James T <jtara@tuta.io>
2017-06-11 19:34:27 -0700

    :bird:Tweet (text) classification successful

--- run.py
+++ run.py
@@ -3,5 +3,12 @@ from src.data_scripts.manual_classification import ManualClassification
 
 
 if __name__ == "__main__":
-    cls = TweetClassifier()
+    clf = TweetClassifier()
+    doc1 = """Earn 3 Fantasy Point Boosters for completing 1 First Win of the Day!\n
+    http://www.smitegame.com/team-booster-weekend-june-9-11 …"""
+    doc2 = """The Joki's on you! Unless you take advantage of 25% off Scarlet Court Chests - on sale now!
+    pic.twitter.com/H58lx7r8xt"""
+
+    print(clf.classify(doc1))
+    print(clf.classify(doc2))
     # ManualClassification(twitter_screen_name="SmiteGame", search_keywords=["FWOTD"])
--- src/utility/load_data.py
+++ src/utility/load_data.py
@@ -1,65 +1,47 @@
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import SGDClassifier
-import numpy as np
+from sklearn import metrics
 import json
-import twitter
-from src.settings import *
+
 
 class TweetClassifier:
-    def __init__(self):
+    def __init__(self, data_file_name="SmiteGame_classified_data.json"):
         self.x_train_tfidf = None  # x-values for data training (term freq, inverse doc freq)
         self.clf = None  # classifier
-        self._load_fit()
+        self.data_file_name = data_file_name  # file containing a list of dictionaries with 'text' and 'category' keys
 
-    def _load_fit(self):
-        # generator that yields a datum from the pre-classified dataset
-        def get_documents(file_name="SmiteGame_classified_data.json", dict_key='text'):
-            with open(file_name, 'r') as f:
-                docs = json.loads(f.read())
-            for doc in docs:
-                yield doc[dict_key] if dict_key == 'text' else doc[dict_key][0]
+        self._load_and_fit()
+        self.print_metrics()
 
-        # for x-values
-        count_vect = CountVectorizer()
-        x_train_counts = count_vect.fit_transform(get_documents())  # get dict of word count in each document
-        print(x_train_counts.shape)  # shape is the numb of docs and numb of unique words throughout all docs
-
-        # term frequency times inverse document frequency (helps docs that spam certain words & downscale common words)
-        tf_transformer = TfidfTransformer()
-        self.x_train_tfidf = tf_transformer.fit_transform(x_train_counts)
-
-        # for y-values
-        count_vect = CountVectorizer()
-        y_train_counts = count_vect.fit_transform(get_documents(dict_key='category'))
-        print(y_train_counts.shape)
-
-        tf_transformer = TfidfTransformer()
-        y_train_tfidf = tf_transformer.fit_transform(y_train_counts)
-
-        text_clf = Pipeline([('vect', CountVectorizer()),
+    def _load_and_fit(self):
+        # takes raw data, applies transforms on it, then puts the transformed data through the estimator / classifier
+        self.clf = Pipeline([('vect', CountVectorizer()),
                              ('tfidf', TfidfTransformer()),
                              ('clf', SGDClassifier(loss='hinge', penalty='l2',
                                                    alpha=1e-3, n_iter=5, random_state=42))])
-        # text_clf = text_clf.fit(
-        #     get_documents(),
-        #     get_documents(dict_key='category'))
-        y_raw_data = [x['category'][0] for x in json.load(open("SmiteGame_classified_data.json", 'r'))]
-        text_clf = text_clf.fit(
-            get_documents(),
-            y_raw_data)
 
-        predicted = text_clf.predict(get_documents())
+        # list of documents (each document is a body of text)
+        self.x_raw_data = [d['text'] for d in json.load(open(self.data_file_name, 'r'))]
+        # list of classification for each document
+        self.y_raw_data = [d['category'][0] for d in json.load(open(self.data_file_name, 'r'))]
+        self.clf = self.clf.fit(
+            self.x_raw_data,
+            self.y_raw_data)
 
-        total = 0
-        for prediction, real_answer in zip(predicted, y_raw_data):
-            total += prediction == real_answer
-            print(prediction == real_answer)
-        print("accuracy = {}".format(total / len(predicted)))
-        # fit data using Multinomial Naive Bayes
-        # self.clf = MultinomialNB().fit(self.x_train_tfidf, y_train_counts)
+        # extract y-feature (categories) names
+        v = CountVectorizer()
+        v.fit_transform(self.y_raw_data)
+        self.target_names = v.get_feature_names()
 
+    def print_metrics(self):
+        predicted = self.clf.predict(self.x_raw_data)
+        print(metrics.classification_report(self.y_raw_data, predicted, target_names=self.target_names))
+
+    def classify(self, document):
+        if not isinstance(document, list):
+            document = [document]
+        return self.clf.predict(document)
 
 if __name__ == "__main__":
     pass
