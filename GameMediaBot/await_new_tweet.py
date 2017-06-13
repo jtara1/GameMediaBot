aa881df70d48cd8af52220c8f907a693f86b95d4
James T <jtara@tuta.io>
2017-06-13 13:20:53 -0700

    forgot to add files for prev commit; added docstrings to the important classes

---
+++ GameMediaBot/await_new_tweet.py
@@ -0,0 +1,94 @@
+import asyncio
+import twitter
+import json
+import logging
+from GameMediaBot.settings import *
+from GameMediaBot.utility.general import get_first_x_words
+
+
+class AwaitNewTweet:
+    """ Monitors an account for new tweets. Classifies the new tweets. Retweets the tweets if they're classified as
+        a trigger tweet.
+    
+        Args:
+            classifier (TweetClassifier):
+                An estimator that's been trained on a dataset and ready to classify a document by calling classify(doc)
+                on it
+            trigger_targets (list of str):
+                If classifier classifies as a str found in trigger_targets, it retweets (or triggers)
+            twitter_screen_name (str):
+                The username of the twitter account we're monitoring 
+            last_id_file (str):
+                File path pointing to file that contains a JSON dictionary containing keys of screen_names with values
+                of the last id processed for the screen_name
+            poll_rate (int or float):
+                The time (seconds) between cycles in the event loop that checks if a new tweet was tweeted
+    """
+    def __init__(self,
+                 classifier,
+                 trigger_targets,
+                 twitter_screen_name,
+                 last_id_file="last_ids.json",
+                 poll_rate=10):
+        self.api = twitter.Api(consumer_key=consumer_key,
+                               consumer_secret=consumer_secret,
+                               access_token_key=access_token_key,
+                               access_token_secret=access_token_secret)
+        self.twitter_screen_name = twitter_screen_name.lower()
+        self.last_ids_file = last_id_file
+        self.last_ids = json.load(open(last_id_file, 'r'))
+
+        if self.twitter_screen_name not in self.last_ids:
+            self._define_new_most_recent_tweet()
+        else:
+            self.most_recent_tweet_id = self.last_ids[twitter_screen_name]
+
+        self.clf = classifier
+        self.trigger_targets = trigger_targets
+        self.poll_rate = poll_rate
+        self.loop = None
+        self.log = logging.getLogger("AwaitNewTweet")
+
+    def _define_new_most_recent_tweet(self):
+        """ Update the JSON file and the dictionary in case this twitter_screen_name has no defined last tweet id """
+        self.most_recent_tweet_id = self.api.GetUserTimeline(screen_name=self.twitter_screen_name, count=2)[1].id
+        self.last_ids[self.twitter_screen_name] = self.most_recent_tweet_id
+
+    def await(self):
+        """ Event loop that will BLOCK FOREVER checking for new tweets then sending them down if new """
+        async def wait_for_new_tweet():
+            while True:
+                statuses = self.api.GetUserTimeline(
+                    screen_name=self.twitter_screen_name,
+                    since_id=self.most_recent_tweet_id)
+                if len(statuses) > 0:
+                    self._process_new_tweets(statuses)
+                await asyncio.sleep(self.poll_rate)
+
+        self.loop = asyncio.get_event_loop()
+        self.loop.run_until_complete(wait_for_new_tweet())
+
+    def stop(self):
+        """ Stop the event loop """
+        if self.loop is not None:
+            self.loop.stop()
+            self.loop.close()
+
+    def _process_new_tweets(self, statuses):
+        """ Retweet the tweets from statuses if they're classified as a trigger tweet/target 
+            Args:
+                statuses (list of twitter.model.Status):
+                    Iterate through these, classifying each
+        """
+        for s in statuses:
+            # if classification of tweet text is in trigger_targets
+            if self.clf.classify(s.text)[0] in self.trigger_targets:
+                print("Retweeting: " + get_first_x_words(s.text, 10) + " ...")
+                self.api.PostRetweet(s.id)  # retweet this tweet
+            else:
+                self.log.debug("Not retweeting: id={}, {} ...".format(s.id, get_first_x_words(s.text, 10)))
+
+        # update dict and json file with most recent twitter id processed
+        self.most_recent_tweet_id = statuses[-1].id
+        self.last_ids[self.twitter_screen_name] = self.most_recent_tweet_id
+        json.dump(self.last_ids, open(self.last_ids_file, 'w'))
---
+++ GameMediaBot/tweet_classifier.py
@@ -0,0 +1,79 @@
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import SGDClassifier
+from sklearn import metrics
+import json
+
+
+class TweetClassifier:
+    """ Automatically does the following given a file_name for the dataset to train classifier:
+        1. Load data from a JSON file formatted as list of dictionaries:
+        Example:
+        [
+          {
+            "id": 873190710291431424,
+            "text": "Get Boosted! This weekend, complete a FWOTD to earn 3 Team Boosters!\n\n
+                https://t.co/KRylCKb1AZ https://t.co/LjGRReHAib",
+            "category": ["fwotd"]
+          }
+        ]
+        2. The x vector of raw data is the list of "text" value from each dict, and the y vector of raw data is
+        the "category" value from each dict. (These vectors are both parallel of course)
+        3. Raw data lists are passed through the sklearn Pipeline which transforms the data into ndarray to be 
+        trained.
+        
+        Ready to classify a new document or test classification with the same x-values used to train it via
+        print_metrics method.
+        This class is more or less the boilerplate code from 
+        http://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
+        It has 100% accuracy so far with my training dataset of 218 tweets for @SmiteGame
+        
+        Args:
+            data_file_name (str):
+                Name of the file containing the dataset which is used to train the classifier
+    """
+    def __init__(self, data_file_name="SmiteGame_classified_data.json"):
+        self.clf = None  # classifier
+        self.data_file_name = data_file_name  # file containing a list of dictionaries with 'text' and 'category' keys
+
+        self._load_and_fit()
+
+    def _load_and_fit(self):
+        """ Load data from file via JSON and pass the data through the Pipeline """
+        # takes raw data, applies transforms on it, then trains the estimator / classifier with the data
+        self.clf = Pipeline([('vect', CountVectorizer()),
+                             ('tfidf', TfidfTransformer()),
+                             ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                                                   alpha=1e-3, n_iter=5, random_state=42))])
+
+        # list of documents (each document is a body of text)
+        self.x_raw_data = [d['text'] for d in json.load(open(self.data_file_name, 'r'))]
+        # list of classification for each document
+        self.y_raw_data = [d['category'][0] for d in json.load(open(self.data_file_name, 'r'))]
+        self.clf = self.clf.fit(
+            self.x_raw_data,
+            self.y_raw_data)
+
+        # extract y-feature (categories) names
+        v = CountVectorizer()
+        v.fit_transform(self.y_raw_data)
+        self.target_names = v.get_feature_names()
+
+    def print_metrics(self):
+        predicted = self.clf.predict(self.x_raw_data)
+        print(metrics.classification_report(self.y_raw_data, predicted, target_names=self.target_names))
+
+    def classify(self, document):
+        """ Classify a document (body of text) using the classifier that was trained
+            Args:
+                document (str):
+                    Any text that'll be classified as a category.
+            Returns (list of str):
+                The list of the names of the categories this document has been classified as.
+        """
+        if not isinstance(document, list):
+            document = [document]
+        return self.clf.predict(document)
+
+if __name__ == "__main__":
+    pass
