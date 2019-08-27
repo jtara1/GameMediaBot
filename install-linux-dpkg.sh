52b8e2e7c92c6696f9bbcfd93c12d6b4b9da439d
James T <github.jtara1@gmail.com>
2019-08-27 01:56:05 -0700

    add: install script for deps

---
+++ install-linux-dpkg.sh
@@ -0,0 +1,7 @@
+sudo apt install build-essential python3-dev -y
+sudo apt install python3-pip python3-numpy -y
+
+sudo apt-get install python3-pil python3-matplotlib python3-pygraphviz -y
+sudo apt-get install default-jdk
+
+pip3 install --user -r requirements.txt
